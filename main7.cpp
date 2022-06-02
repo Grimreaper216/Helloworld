f5f2662516fdf52e7361ca939bc908d0f4d6012f
kcs9303 <96504554+kcs9303@users.noreply.github.com>
2022-06-02 16:56:58 +0900

    복사본

---
+++ main7.cpp
@@ -0,0 +1,94 @@
+/*
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+	/*
+	cout << "* * * * * * * * * *" << endl;
+	cout << "*                 *" << endl;
+	cout << "*                 *" << endl;
+	cout << "*                 *" << endl;
+	cout << "*                 *" << endl;
+	cout << "*                 *" << endl;
+	cout << "*                 *" << endl;
+	cout << "*                 *" << endl;
+	cout << "*                 *" << endl;
+	cout << "* * * * * * * * * *" << endl;
+
+	int a, b = { 0 };
+	cout << "A is ?";
+	cin >> a;
+	cout << "B is ?";
+	cin >> b;
+
+	cout << "16진수(A + b) = " << hex << uppercase << a + b << nouppercase << endl;//uppercase 대문자 변환
+	
+
+	int a, b = { 0 };
+	cout << "A is ?";
+	cin >> a;
+	cout << "B is ?";
+	cin >> b;
+
+
+	//cout << boolalpha << (a - b > 0) << noboolalpha << endl;
+
+	if (a - b > 0)
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+	
+
+	int a, b, c = { 0 };
+	cout << "A is ?";
+	cin >> a;
+	cout << "B is ?";
+	cin >> b;
+	cout << "C is ?";
+	cin >> c;
+
+	if (a + b > c)
+	{
+		cout << a + b << endl;
+	}
+	else
+	{
+		cout << c << endl;
+	}
+
+	{
+		int a = 0b10101010;
+		int b = 0111;
+		int c = 0x11;
+
+		cout << a + b - c << endl;
+
+	}
+
+	{
+		int a, b, c = { 0 };
+		cout << "A is ?";
+		cin >> a;
+		cout << "B is ?";
+		cin >> b;
+		cout << "C is ?";
+		cin >> c;
+		
+		if (c > a && a > b)
+		{
+			cout << "true" << endl;
+		}
+		else
+		{
+			cout << "false" << endl;
+		}
+	}
+}
+*/
---
+++ main8.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*
+	int score = { 0 };
+	cin >> score;
+
+	if (score >= 90)
+	{
+		cout << "A" << endl;
+	}
+	else if (90 > score && score >= 80)
+	{
+		cout << "B" << endl;
+	}
+	else if (80 > score && score >= 70)
+	{
+		cout << "C" << endl;
+	}
+	else if (70 > score && score >= 60)
+	{
+		cout << "D" << endl;
+	}
+	else if (60 > score && score >= 50)
+	{
+		cout << "E" << endl;
+	}
+	else
+	{
+		cout << "F" << endl;
+
+	}
+	// else if 는 if 와 else 사이에 추가 조건을 넣기위해 사용
+	{
+		score /= 10;
+
+		switch (score)
+		{
+		case 10:
+		case 9:
+			cout << "A" << endl;
+			break;
+		case 8:
+			cout << "B" << endl;
+			break;
+		case 7:
+			cout << "C" << endl;
+			break;
+		case 6:
+			cout << "D" << endl;
+			break;
+		case 6:
+			cout << "E" << endl;
+			break;
+		default:
+			cout << "F" << endl;
+			break;
+		}
+
+	}
+	{
+		int a, b = { 0 };
+		cout << "A is ?";
+		cin >> a;
+		cout << "B is ?";
+		cin >> b;
+
+		bool result = (a - b > 0);
+		//cout << boolalpha << (a - b > 0) << noboolalpha << endl;
+
+		switch (result)
+		{
+		case true:
+			cout << "true" << endl;
+			break;
+
+		default:
+			cout << "false" << endl;
+			break;
+		}
+
+
+	}
+	{
+		int a, b, c = { 0 };
+		cout << "A is ?";
+		cin >> a;
+		cout << "B is ?";
+		cin >> b;
+		cout << "C is ?";
+		cin >> c;
+
+		bool result = (a + b > c);
+		switch (result)
+		{
+		case true:
+				cout << "a + b =" << a + b << endl;
+				break;
+
+			default:
+				cout << "c = " << c << endl;
+				break;
+		}
+
+	}
+	{
+		int a = { 0 };
+		cout << "1 ~ 4 사이에 숫자를 선택하시오.";
+		cin >> a;
+		switch (a)
+		{
+		case 1:
+			cout << "R E D" << endl;
+			break;
+		case 2:
+			cout << "B L U E" << endl;
+			break;
+		case 3:
+			cout << "B L A C K" << endl;
+			break;
+		case 4:
+			cout << "W H I T E" << endl;
+			break;
+
+		default:
+			cout << "해당없음 " << endl;
+			break;
+
+		}
+	{
+		int a = { 0 };
+		cout << "주민번호 뒤 7자리 입력";
+		cin >> a;
+
+		a /= 1000000;
+		switch ( a )
+		{
+		case 1:
+			cout << "남자" << endl;
+			__fallthrough;
+		case 3:
+			cout << "남자" << endl;
+			break;
+
+		case 2:
+			cout << "남자" << endl;
+			__fallthrough;
+		case 4:
+			cout << "여자" << endl;
+			break;
+		default:
+			cout << "외계인 " << endl;
+			break;
+		}
+		*/
+	}
+
+
+
+
